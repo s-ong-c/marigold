c7e344851d57fd960568a0419fd00c2b4e23c345
sarahgm <sarah.gosmann@reservix.de>
2022-08-11 16:30:59 +0200

    it works... kinda

--- docs/next.config copy.mjs
+++ docs/next.config copy.mjs
@@ -1,15 +1,6 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import pkg from './package.json' assert { type: 'json' };
-import nextMDX from '@next/mdx';
-
-// Plugins
-import rehypeAutolinkHeadings from 'rehype-autolink-headings';
-import rehypeSlug from 'rehype-slug';
-import remarkGfm from 'remark-gfm';
-
-import * as remarkCodeDemo from './src/plugins/remark-code-demo.js';
-import * as rehypeTableOfContents from './src/plugins/rehype-toc.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -46,27 +37,4 @@ const config = {
   },
 };
 
-export const withMDX = nextMDX({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [
-      remarkGfm,
-      remarkCodeDemo,
-      {
-        demoPath: path.join(process.cwd(), 'src', 'demos'),
-        wrapperComponent: 'Preview',
-      },
-    ],
-    rehypePlugins: [
-      rehypeSlug,
-      [rehypeAutolinkHeadings, { behavior: 'wrap' }],
-      [rehypeTableOfContents, { tocSelector: '#toc' }],
-    ],
-    providerImportSource: '@mdx-js/react',
-  },
-});
-
-export default withMDX({
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-  ...config,
-});
+export default config;
---
+++ docs/src/plugins/rehype-toc.js
@@ -0,0 +1,56 @@
+import { visit } from 'unist-util-visit';
+
+import { Element, Root, Text } from 'hast';
+import { Transformer } from 'unified';
+
+/** @type {Options: {tocSelector: string} } */
+
+export const rehypeTableOfContents = options => {
+  return tree => {
+    const links = [];
+
+    visit(tree, 'element', node => {
+      if (node.tagName !== 'h2') {
+        return;
+      }
+      const link = node.children.find(child => child.tagName === 'a');
+      if (link) {
+        links.push(link);
+      }
+    });
+    const data = links.map(link => {
+      return {
+        anchor: link?.properties?.href,
+        title: link?.children[0]?.value,
+      };
+    });
+
+    tree.children.unshift({
+      type: 'mdxJsxFlowElement',
+      name: 'Toc',
+      attributes: [
+        {
+          type: 'mdxJsxAttribute',
+          name: 'selector',
+          value: options.tocSelector,
+        },
+        // this is how the data should be passed but it's not working
+        //
+        // const value = {
+        //   type: 'mdxJsxAttribute',
+        //   name: 'props',
+        //   value: {
+        //     type: 'mdxJsxAttributeValueExpression',
+        //     value: '<json>',
+        //   },
+        // };
+        {
+          type: 'mdxJsxAttribute',
+          name: 'items',
+          value: JSON.stringify(data),
+        },
+      ],
+      children: [],
+    });
+  };
+};
