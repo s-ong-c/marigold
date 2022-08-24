4d64a68786a7426cbb9665edeb2a0de4eba38992
sarahgm <sarahgm@users.noreply.github.com>
2022-08-24 10:22:54 +0000

    Deploying to gh-pages from @ marigold-ui/marigold@655588a53a71150b64e7650d9fc25c359338bd81 🚀

--- _next/static/B4paKS6ATqbE_OqCz6gGe/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/404":["static/chunks/pages/404-0b195aa623e9c1ab.js"],"/_error":["static/chunks/pages/_error-9ad5bd204373e46e.js"],"/[[...slug]]":["static/chunks/pages/[[...slug]]-cd0be392edac6a94.js"],sortedPages:["/404","/_app","/_error","/[[...slug]]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
