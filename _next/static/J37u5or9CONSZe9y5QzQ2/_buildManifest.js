52c07046e1816824e086814aab8cb0915ea457c2
sebald <sebald@users.noreply.github.com>
2022-08-24 09:29:42 +0000

    Deploying to gh-pages from @ marigold-ui/marigold@17c756ec4ba80c6c36f6b43dec53704801a02602 🚀

--- _next/static/J37u5or9CONSZe9y5QzQ2/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/404":["static/chunks/pages/404-0b195aa623e9c1ab.js"],"/_error":["static/chunks/pages/_error-9ad5bd204373e46e.js"],"/[[...slug]]":["static/chunks/pages/[[...slug]]-740779eb10f6036d.js"],sortedPages:["/404","/_app","/_error","/[[...slug]]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
