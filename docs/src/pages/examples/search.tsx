aa5e8051a213ccaec26c48b652adecbc36245079
Sebastian Sebald <sebastian.sebald@reservix.de>
2022-09-20 14:03:48 +0200

    add swr

--- docs/src/pages/examples/search.tsx
+++ docs/src/pages/examples/search.tsx
@@ -19,7 +19,12 @@ import {
   TextField,
 } from '@marigold/components';
 import { Search } from '@marigold/icons';
+import { useState } from 'react';
+import useSWR, { Key, Fetcher } from 'swr';
+
 import { Link, ThemeSelect, useThemeSwitch } from '~/components';
+
+const fetcher: any = url => fetch(url).then(res => res.json());
 
 const Navigation = () => (
   <nav>
@@ -35,32 +40,46 @@ const Navigation = () => (
   </nav>
 );
 
-const SearchForm = () => (
-  <Inline space="small">
-    <form>
+const SearchForm = ({ query, onSubmit }) => (
+  <form onSubmit={onSubmit}>
+    <Inline space="small">
       <TextField
         label="Search"
         description="Type in what you are looking for!"
         placeholder="Search..."
         width="huge"
+        value={query}
       />
       <Button variant="primary" size="small" type="submit">
         <Search /> Search
       </Button>
-    </form>
-  </Inline>
+    </Inline>
+  </form>
 );
 
 export default () => {
   const { current, themes } = useThemeSwitch();
   const theme = (current && themes[current]) || themes.b2b2;
 
+  const [query, setQuery] = useState('');
+
+  const { data, error } = useSWR(
+    `https://swapi.dev/api/people/?search=${query}`,
+    fetcher
+  );
+
+  const handleSubmit = ev => {
+    ev.preventDefault();
+
+    console.log(ev);
+  };
+
   return (
     <MarigoldProvider theme={theme} selector="#search-example">
       <Box id="#search-example">
         <Stack space="xlarge">
           <Navigation />
-          <SearchForm />
+          <SearchForm query={query} onSubmit={handleSubmit} />
           <Aside space="small">
             <div style={{ width: '10em' }}>liste</div>
             <div>details</div>
